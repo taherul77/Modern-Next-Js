428a0f639197433ce0a08e3ccdcca0ce91731967
taherul <sktaherul@atilimited.net>
2024-04-25 11:35:35 +0530

    add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='flex flex-col'>
+    <footer className='flex flex-col text-black-100 mt-5 border-t border-gray-100'>
+      <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+        <div className='flex flex-col justify-start item-start gap-6'>
+          <Image src="/logo.svg" alt='' width={180} height={18}/>
+        </div>
+      </div>
 
     </footer>
   )
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,32 +5,29 @@ import CustomButton from "./CustomButton";
 const Hero = () => {
   const handleScroll = () => {};
   return (
-    <div className="Hero">
-      <div className="flex-1 pt-36 padding-x">
+    <div className="hero">
+      <div className="flex-1 pt-20 padding-x">
         <h1 className="hero__title">
-          Find,Book or Rent a car quickly and easily!
+          Find, book, rent a car—quick and super easy!
         </h1>
+
         <p className="hero__subtitle">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
-          quidem, illum at aut id enim possimus blanditiis doloribus?
-          Recusandae, expedita.
+          Streamline your car rental experience with our effortless booking
+          process.
         </p>
+
         <CustomButton
-          title="Expolore Car"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-5"
           handleClick={handleScroll}
         />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image
-            src="/hero.png"
-            alt="hero"
-            fill
-            className="object-contain"
-          ></Image>
-          <div className="hero__image-overlay"/>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
